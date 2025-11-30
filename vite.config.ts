3faaf133d02cd8310d054729492dfd3c42be85e7
Victor J. David <davidvictor297@gmail.com>
2025-11-30 18:21:56 +0100

    all

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ["lucide-react"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./attached_assets"),
+    },
+  },
+  // server: {
+  //   host: '0.0.0.0',
+  //   port: 5000,
+  //   allowedHosts: true,
+  // },
+});
